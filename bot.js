7b897e120f2afb958481f7202ede9b513872eb1f
chris laframboise <hire.code.seegreatness@gmail.com>
2018-09-19 12:14:08 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.BOT_TOKEN;
 const prefix = "!"; // Set the prefix
-
+const config = require('./config.Json');
 client.on("ready", () => {	
     console.log(`${client.user.tag} Is Active!`);
     client.user.setPresence({game: {name: `with you!`, type: 1}});
@@ -11,57 +11,111 @@ client.on("ready", () => {
 
 });
 
+//Constants 
+const DailyRecently = new Set();
+const talkedRecently = new Set();
+//talkedRecently is the cooldown for all commands plz implement this shit and dont fuck it up :( lmao
 
 
+//variables
+var TalkIntetrval=60000;
+var DailyIntetrval=60000;
+//move these to the config.Json
+
+//objects
 const responseObject = {
   "ayy": "Ayy, lmao!",
   "wat": "Say what?",
   "lol": "roflmaotntpmp",
-  "!Invite":"http://www.tinyurl.com/PokemonGoAITrainer",
-  "!invite":"http://www.tinyurl.com/PokemonGoAITrainer"
+  "prefix":"the prefi is !"
 };
-const talkedRecently = new Set();
+//move this to its own file... call it hmm responseObject.Json
 
 
-//adding new code to replace the current command system
+
+//commands
+
+//this is the new command system DONT FUCK IT UP AGAIN!
   client.on("message", message => {
-	  
-  // This is where we'll put our code.
+  // This is where we put our gay commands please make them not so gay.
 
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    //only get the messages with the prefix
   const command = args.shift().toLowerCase();
-  
+  //change the command toLowerCase
 
 	if (message.content.startsWith(prefix + "profile")||message.content === "+profile") {
-    // Send the user's avatar URL
+  //if the message contains the prefix and the command is "profile"
     message.reply(message.author.avatarURL);
-  }else
-  if(responseObject[message.content]) {
-    message.channel.send(responseObject[message.content]);
-  }else
-   if(message.content.startsWith(prefix + "daily")||message.content === "+daily"){
+    // Send the user's avatar URL and Discord will automaticaly put a preview of the image in chat
+    //upgrade this to embed the Object so we dont have to show the URL like we dont know how to do shit....
 
-       if (talkedRecently.has(message.author.id)) {
+  }else
+  //else if the message does not contains the prefix and the command is "profile"
+  if(responseObject[message.content]) {
+    //if the message does not contain the prefix but contains anything in the Json Array "responseObject" 
+    message.channel.send(responseObject[message.content]);
+    //send the result stored in the Json Array
+  }else
+  //else if that was not entered
+   if(message.content.startsWith(prefix + "daily")||message.content === "+daily"){
+  //check to see if the message contains the prefix and the command is "daily" OR if it contains "+daily"
+       if (DailyRecently.has(message.author.id)) {
+         //if the user ID is in the Array "DailyRecently"
          message.channel.send(message.author + " Wait 1 minute to claim again!");
     
        }else{
-           // the user can type the command ... your command code goes here :)
+         // else if the user is not in the Array "DailyRecently"
            message.reply("you claimed your Daily come back in one minute to claim again!");
-        // Adds the user to the set so that they can't talk for a minute
-        talkedRecently.add(message.author.id);
+          //ping them a response
+        DailyRecently.add(message.author.id);
+          // Adds the user to the set so that they can't talk for a minute
+
         setTimeout(() => {
-          // Removes the user from the set after a minute
-          talkedRecently.delete(message.author.id);
-	message.author.send(message.author + " you may collect your daily now!");
+          // Removes the user from the set after a the set time interval
+          DailyRecently.delete(message.author.id);
+          //also send them a direct message letting them know they can daily again
+        	message.author.send(message.author + " you may collect your daily now!");
 
 
-        }, 60000);
+        }, DailyIntetrval);
+        //DailyIntetrval is a variable PLZ move this to the config file
+        //also this might not work and cause errors :(
     }     
- }else       
+ }else
+    if(message.content.startsWith(prefix + "cash")||message.content === "+cash"){
+  //check to see if the message contains the prefix and the command is "cash" OR if it contains "+cash"
+       if (talkedRecently.has(message.author.id)) {
+         //if the user ID is in the Array "talkedRecently"
+         message.channel.send(message.author + "you just checked your balance come back in one minute to check again!");
+    
+       }else{
+         // else if the user is not in the Array "talkedRecently"
+           message.reply("you have a million dollars :lmfao: ");
+          //ping them a response
+        talkedRecently.add(message.author.id);
+          // Adds the user to the set so that they can't talk for a minute
+
+        setTimeout(() => {
+          // Removes the user from the set after a the set time interval
+          talkedRecently.delete(message.author.id);
+
+        }, TalkIntetrval);
+        //TalkIntetrval is a variable PLZ move this to the config file
+        //also this might not work and cause errors :(
+    }     
+ }else
+  
+
+ //else if the user used the prefix or basicaly sent any kind of message the bot can see \
+ //exit
+ //if it somehow causes an error then send a message saing so instead of crashing like a bitch
   if(message.content===null){
     message.channel.send("Unknown Command");
   }
   });
 
-// THIS  MUST  BE  THIS  WAY
+// THIS  MUST  BE  THIS  WAY 
 client.login(process.env.BOT_TOKEN);
+//dont reveal your token in your fucking code that public *facepalms*
+
